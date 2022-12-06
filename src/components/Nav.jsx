e9bda4b981d7a85da358c6d1bd448997559a7297
26june <gj.abordo1@gmail.com>
2022-12-06 11:20:22 +0000

    add home button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,14 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { LoggedInAs } from "../context/LoggedInAs";
 
 export default function Nav() {
   const { loggedInAs } = useContext(LoggedInAs);
   const { username, name, avatar_url } = loggedInAs;
-  return <div className="nav-container">{username}</div>;
+  return (
+    <div className="nav-container">
+      <Link to="/">Home</Link>
+      {username}
+    </div>
+  );
 }
