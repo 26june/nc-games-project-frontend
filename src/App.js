c87f2fc09b81b69db4b1c4ec88f06dd4d7a4bae8
26june <gj.abordo1@gmail.com>
2022-12-09 13:29:24 +0000

    set a default user

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Login from "./components/_Login";
 import { LoggedInAs } from "./context/LoggedInAs";
 
 function App() {
-  const [loggedInAs, setLoggedInAs] = useState(null);
+  const [loggedInAs, setLoggedInAs] = useState({
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
+  });
 
   return (
     <LoggedInAs.Provider value={{ loggedInAs, setLoggedInAs }}>
