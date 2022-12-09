e119d3f512c5b1c012036fca8f514bc41d5f3480
26june <gj.abordo1@gmail.com>
2022-12-09 13:30:01 +0000

    remove nested routes

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getReviewsById } from "../api/api";
 import CommentSection from "./CommentSection";
 import Loading from "./loading/Loading";
