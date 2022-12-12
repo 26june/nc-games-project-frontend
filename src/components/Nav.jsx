742977cc76ca6a23d51c5e21bec120034692a0b9
26june <gj.abordo1@gmail.com>
2022-12-12 12:23:25 +0000

    add mui components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,68 +1,94 @@
-import React, { useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { getCategories } from "../api/api";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { LoggedInAs } from "../context/LoggedInAs";
-import { SelectedCategory } from "../context/SelectedCategory";
+
 import "../style/Nav.css";
+
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  IconButton,
+} from "@mui/material";
+
+//Icons
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SwitchAccountOutlinedIcon from "@mui/icons-material/SwitchAccountOutlined";
 
 export default function Nav() {
   const { loggedInAs, setLoggedInAs } = useContext(LoggedInAs);
-  const {
-    categoriesState,
-    setCategoriesState,
-    selectedCategory,
-    setSelectedCategory,
-  } = useContext(SelectedCategory);
-  const { username } = loggedInAs;
-  // { name, avatar_url} can be deconstructed from here
+
+  const { username, name, avatar_url } = loggedInAs;
+
+  const [drawerState, setDrawerState] = useState(false);
 
   const navigate = useNavigate();
 
-  function handleCategoryChange(event) {
-    event === "All Categories"
-      ? setSelectedCategory("")
-      : setSelectedCategory(event);
-
-    navigate(`categories/${event}`);
-  }
-
-  useEffect(() => {
-    getCategories().then((categories) => {
-      setCategoriesState(categories);
-    });
-  }, [setCategoriesState]);
-
   return (
-    <div className="nav-container">
-      <Link to="/">Home</Link>
-      <div>
-        <label htmlFor="categories">Select a Category</label>
-        <select
-          name="categories"
-          id="categories"
-          onChange={(event) => {
-            handleCategoryChange(event.target.value);
-          }}
-          value={selectedCategory}
-        >
-          <option>All Categories</option>
-          {categoriesState.map((category) => {
-            return (
-              <option key={category.slug} value={category.slug}>
-                {category.slug}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <p>{username}</p>
-      <button
-        onClick={() => {
-          setLoggedInAs(null);
+    <div className="test-container">
+      <Box
+        sx={{
+          width: "100%",
+          height: "10vh",
+          //   backgroundColor: "white",
+          color: "primary.main",
         }}
+        display="flex"
+        justifyContent={"space-between"}
+        padding={"10px"}
+        alignItems={"center"}
       >
-        Swtich User
-      </button>
+        <IconButton
+          onClick={() => {
+            navigate("/");
+          }}
+          color="primary"
+        >
+          <HomeOutlinedIcon></HomeOutlinedIcon>
+        </IconButton>
+        NC GAMES REVIEW
+        <IconButton
+          color="primary"
+          onClick={() => {
+            setDrawerState(true);
+          }}
+        >
+          <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon>
+        </IconButton>
+        <Drawer
+          open={drawerState}
+          anchor="right"
+          onClose={() => {
+            setDrawerState(false);
+          }}
+        >
+          <div className="user-drawer-container">
+            <Avatar
+              src={avatar_url}
+              alt={name}
+              sx={{ border: "1px solid black" }}
+            ></Avatar>
+            <h3>{name}</h3>
+            <h4>{username}</h4>
+            <Divider sx={{ color: "primary.main" }}></Divider>
+            <Button
+              startIcon={
+                <SwitchAccountOutlinedIcon></SwitchAccountOutlinedIcon>
+              }
+              variant="contained"
+              onClick={() => {
+                setLoggedInAs(null);
+              }}
+            >
+              Switch User
+            </Button>
+          </div>
+        </Drawer>
+      </Box>
     </div>
   );
 }
